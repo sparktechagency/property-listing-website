b8a7afd0cbb49747255a93eb8865a2b4823a9562
Naziya Sultana Mithila <121658415+MithilaKhan@users.noreply.github.com>
2025-06-30 16:22:35 +0600

    complete

--- src/redux/base/baseApi.ts
+++ src/redux/base/baseApi.ts
@@ -5,7 +5,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://148.66.158.106:5000/api/v1" ,
+    baseUrl: "https://api.ambitiousbizxchange.com/api/v1" ,
       // baseUrl: "http://10.0.80.75:5005/api/v1" , 
       prepareHeaders: (headers) => {
         const token = GetLocalStorage("accessToken");
@@ -21,5 +21,5 @@ export const baseApi = createApi({
 
 // export const imageUrl = "http://10.0.80.75:5005/";
 // export const socketURL = "http://10.0.80.75:5005";
-export const imageUrl = "http://148.66.158.106:5000/"; 
-export const socketURL  = "http://148.66.158.106:5000"; 
+export const imageUrl = "https://api.ambitiousbizxchange.com/"; 
+export const socketURL  = "https://api.ambitiousbizxchange.com"; 
